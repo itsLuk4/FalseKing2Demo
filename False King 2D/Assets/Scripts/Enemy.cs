a67c42c215f16fec3e6f5f541825b592e7533bac
Luka Živko <luka.zivko9@gmail.com>
2023-05-07 18:23:35 +0200

    Added enemies and made them running.

---
+++ False King 2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float enemyRunSpeed = 5f;
+
+    Rigidbody2D enemyRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingLeft())
+        {
+            enemyRigidBody.velocity = new Vector2(-enemyRunSpeed, 0f);
+        }
+        else
+        {
+            enemyRigidBody.velocity = new Vector2(enemyRunSpeed, 0f);
+        }
+        
+    }
+
+    // we use this trigger exit so the enemy flips
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        flipSprite();
+    }
+
+    private void flipSprite()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(enemyRigidBody.velocity.x), 1f);
+    }
+
+    // this is a boolean that will transform localscale.x and it will check if that scale is bigger or smaller than 0
+    private bool IsFacingLeft()
+    {
+        return transform.localScale.x > 0;
+
+    }
+}
